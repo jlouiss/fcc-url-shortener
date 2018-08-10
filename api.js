f1310b7210587c12bd58281e1b634eb389108d8e
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-08-10 22:23:38 +0200

    added default error messages and fixed mongoose bug

--- api.js
+++ api.js
@@ -5,13 +5,16 @@ const router = express.Router()
 
 mongoose.connect(process.env.MONGODB_URL)
 
-const ShortURL = new mongoose.model(
+const ShortURL = mongoose.model(
   'ShortURL',
   new mongoose.Schema({
     original_url: String,
     short_url: Number
   })
 )
+
+const invalidUrlResponse = { error: 'invalid URL' }
+const urlDoesntExistResponse = { error: 'No short url for given input' }
 
 router.post('/new', (req, res) => {
   console.log(req.body)
